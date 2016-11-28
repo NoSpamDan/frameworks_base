f816e2577e2a3090daf9ded65e47dac9806bc4a1
Alex Cruz <mazdarider23@gmail.com>
2016-11-28 05:11:00 +0100

    QS tile titles visibility [1/2]
    ----
    abc ezio84:
    don't call textVisibility on each handleStateChanged
    but expose it through TileLayout mRecords and call it only when
    changing the settings
    
    fix also title visibility not being refreshed after a
    configuration change
    ----
    
    Pretty simple mod to remove tile titles if and when the titles become
    something that you don't want. A good case for this is if you set 7 or more
    columns across, the titles move down to 2 lines and is just looks awful IMO
    
    Before - https://i.imgur.com/G3ICaqb.png
    After - https://i.imgur.com/Hb0J1XW.png
    
    PS2 - Thanks to @maxwen for the know how to adjust the gap when titles
    are removed
    
    Change-Id: I46fa9ebb01bd36f6589b0c6231a80dbc87fe1ca4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4512,6 +4512,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
 
         /**
+         * Whether to display qs tile titles in the qs panel
+         * @hide
+         */
+        public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
