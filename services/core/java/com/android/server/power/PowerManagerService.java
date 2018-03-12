TeamNexus/android_frameworks_base

PowerManagerService: prevent sending inappropriate DOZING power-hint
Change-Id: I4b76a887411abf4b230cd69a7c1460d9e761a216

services/core/java/com/android/server/power/PowerManagerService.java
@@ -2308,9 +2308,6 @@ public final class PowerManagerService extends SystemService
             if (startDreaming) {
                 mDreamManager.stopDream(false /*immediate*/);
                 mDreamManager.startDream(wakefulness == WAKEFULNESS_DOZING);
-
-                // notify power-HAL we transition into dozing/dreaming
-                powerHintInternal(NexusPowerHint.DOZING, 1);
             }
             isDreaming = mDreamManager.isDreaming();
         } else {
@@ -2327,6 +2324,9 @@ public final class PowerManagerService extends SystemService
                 } else {
                     Slog.i(TAG, "Dreaming...");
                 }
+
+                // notify power-HAL we transition into dozing/dreaming
+                powerHintInternal(NexusPowerHint.DOZING, 1);
             }
 
             // If preconditions changed, wait for the next iteration to determine

NoSpamDan/frameworks_base

services/core/java/com/android/server/power/PowerManagerService.java
@@ -2373,6 +2373,9 @@ public final class PowerManagerService extends SystemService
                 } else {
                     Slog.i(TAG, "Dreaming...");
                 }
+
+                // notify power-HAL we transition into dozing/dreaming
+                powerHintInternal(CandyPowerHint.DOZING, 1);
             }
 
             // If preconditions changed, wait for the next iteration to determine