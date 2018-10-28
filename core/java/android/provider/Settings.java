c6fc5fc745d802b6be6bd75c1fcaa349d454785e
ezio84 <brabus84@gmail.com>
2018-10-28 10:44:33 +0100

    Add CustomDoze
    other commits are in the CustomDoze tree, Settings, device tree
    
    needs also https://github.com/ezio84/abc_frameworks_base/commit/a889512dc25ed6d9e7f527b43ae0da81f53b9092#diff-d076d2c9ae5697840f02efd22836dc65R73
    
    Change-Id: I4817eaf7f1995c2e8df7835f4f68fc6e2a59e710

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4586,6 +4586,18 @@ public final class Settings {
         public static final String AMBIENT_BATTERY_PERCENT = "ambient_battery_percent";
 
         /**
+         * Custom Ambient handwave gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_HANDWAVE_GESTURE = "custom_ambient_handwave_gesture";
+
+         /**
+         * Custom Ambient pocketmode gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
