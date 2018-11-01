eb3415d2116e4f423cb749a18cb1b1b532e3d350
ezio84 <brabus84@gmail.com>
2018-11-01 11:20:55 +0100

    Full nav gesture: home action on the whole bar and doubletap2sleep [1/2]
    all this stuff comes from what we did on dui (waiting for it on P!)
    so huge thanks to @bigrushdog
    
    Change-Id: I19085f97997c4c3e24ec8cc0004fa5e0ddac1512

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -683,5 +683,34 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         void setListening(boolean listening);
 
         default void setExpansion(float expansion) {}
+
+        boolean isShowTitles();
+    }
+
+    private void configureTile(QSTile t, QSTileView v) {
+        if (mTileLayout != null) {
+            if (t.isDualTarget()) {
+                if (!mTileLayout.isShowTitles()) {
+                    v.setOnLongClickListener(view -> {
+                        t.secondaryClick();
+                        return true;
+                    });
+                } else {
+                    v.setOnLongClickListener(view -> {
+                        t.click();
+                        t.longClick();
+                        return true;
+                    });
+                }
+            }
+        }
+    }
+
+    public void updateSettings() {
+        if (mTileLayout != null) {
+            for (TileRecord r : mRecords) {
+                configureTile(r.tile, r.tileView);
+            }
+        }
     }
 }
