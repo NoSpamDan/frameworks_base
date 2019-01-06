omnirom/android_frameworks_base_old

SystemUI: special feature improvements
Change-Id: I78ee9f60bedbb27101df4cb3f9e97c42461c8ab0

packages/SystemUI/src/com/android/systemui/omni/GifImageView.java
@@ -1,0 +1,93 @@
+package com.android.systemui.omni;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.net.Uri;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class GifImageView extends View {
+
+    private InputStream mInputStream;
+    private Movie mMovie;
+    private int mWidth, mHeight;
+    private long mStart;
+    private Context mContext;
+
+    public GifImageView(Context context) {
+        super(context);
+        this.mContext = context;
+    }
+
+    public GifImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GifImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.mContext = context;
+        if (attrs.getAttributeName(1).equals("background")) {
+            int id = Integer.parseInt(attrs.getAttributeValue(1).substring(1));
+            setGifImageResource(id);
+        }
+    }
+
+    private void init() {
+        setFocusable(true);
+        mMovie = Movie.decodeStream(mInputStream);
+        mWidth = mMovie.width();
+        mHeight = mMovie.height();
+
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(mWidth, mHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        long now = SystemClock.uptimeMillis();
+
+        if (mStart == 0) {
+            mStart = now;
+        }
+
+        if (mMovie != null) {
+
+            int duration = mMovie.duration();
+            if (duration == 0) {
+                duration = 1000;
+            }
+
+            int relTime = (int) ((now - mStart) % duration);
+
+            mMovie.setTime(relTime);
+
+            mMovie.draw(canvas, 0, 0);
+            invalidate();
+        }
+    }
+
+    public void setGifImageResource(int id) {
+        mInputStream = mContext.getResources().openRawResource(id);
+        init();
+    }
+
+    public void setGifImageUri(Uri uri) {
+        try {
+            mInputStream = mContext.getContentResolver().openInputStream(uri);
+            init();
+        } catch (FileNotFoundException e) {
+            Log.e("GIfImageView", "File not found");
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -43,7 +43,7 @@ import com.android.systemui.R;
 import com.android.systemui.R.id;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.omni.OmniSystemUIUtils;
-import com.android.systemui.omni.xFallView.views.XFallView;
+import com.android.systemui.omni.GifImageView;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.qs.customize.QSCustomizer;
 import com.android.systemui.statusbar.CommandQueue;
@@ -90,7 +90,7 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
     private boolean mSecureExpandDisabled;
     private View mFunImage;
     private View mFunImageContainer;
-    private XFallView mXFallView;
+    private GifImageView mFunBgImage;
     private boolean mForceHideFun;
     private boolean mForceShowFun;
     private ViewPropertyAnimator mFunAnimation;
@@ -116,7 +116,7 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
         mContainer = view.findViewById(id.quick_settings_container);
         mFunImage = view.findViewById(R.id.qs_fun_image);
         mFunImageContainer = view.findViewById(R.id.qs_fun_image_container);
-        mXFallView = view.findViewById(R.id.qs_fun_background);
+        mFunBgImage = view.findViewById(R.id.qs_fun_background);
         mQSDetail.setQsPanel(mQSPanel, mHeader, (View) mFooter);
         mQSAnimator = new QSAnimator(this, mQuickQSPanel, mQSPanel);
 
@@ -319,7 +319,6 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             showFunImage();
         } else {
             mFunImageContainer.setVisibility(View.GONE);
-            mXFallView.stopFall();
             if (mFunAnimation != null) {
                 mFunAnimation.cancel();
             }
@@ -514,34 +513,14 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             return;
         }
         mFunImageContainer.setVisibility(View.VISIBLE);
-        if (!mQsExpanded) {
-            mFunImage.setVisibility(View.VISIBLE);
-            mFunImage.setTranslationX(0);
-            mFunAnimation = mFunImage.animate()
-                .translationX(mQSPanel.getWidth() - mFunImage.getWidth())
-                .setInterpolator(Interpolators.LINEAR)
-                .setStartDelay(0)
-                .setDuration(5000)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        mFunImage.setVisibility(View.GONE);
-                    }
-                });
-            mFunAnimation.start();
-        }
-        mXFallView.startFall();
-        mXFallView.animate()
-            .setDuration(500)
-            .alpha(1)
-            .start();
+        mFunBgImage.setGifImageResource(R.raw.fireworks);
     }
 
     private boolean isFunEnabled() {
         if (!OmniSystemUIUtils.isFunEnabled()) {
             return false;
         }
-        if(OmniSystemUIUtils.isXMasFunEnabled()) {
+        if(OmniSystemUIUtils.isNewYearsFunEnabled()) {
             if (mForceHideFun) {
                 return false;
             } else {
@@ -565,19 +544,10 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             mForceShowFun = newValue != null && Integer.parseInt(newValue) == 1;
         }
         if (isFunEnabled()) {
-            mXFallView.setOnLongClickListener(new View.OnLongClickListener(){
-                @Override
-                public boolean onLongClick(View v) {
-                    if (!mKeyguardShowing) {
-                        OmniSystemUIUtils.startXMasFun(getContext());
-                    }
-                    return true;
-                }
-            });
-            mXFallView.setOnClickListener(new View.OnClickListener(){
+            mFunBgImage.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
-                    final MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.ho_ho_ho);
+                    final MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.fireworks_sound);
                     mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                         @Override
                         public void onCompletion(MediaPlayer mp){

packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -100,7 +100,7 @@ public class TunerFragment extends PreferenceFragment {
 
         Preference preference = findPreference(KEY_QS_SHOW_FUN);
         if (preference != null) {
-            if (!OmniSystemUIUtils.isXMasFunEnabled()) {
+            if (!OmniSystemUIUtils.isNewYearsFunEnabled()) {
                 ((PreferenceScreen) findPreference(KEY_QS_CATEGORY)).removePreference(preference);
             }
         }
@@ -108,7 +108,7 @@ public class TunerFragment extends PreferenceFragment {
         final Preference ts = findPreference(KEY_QS_FORCE_SHOW_FUN);
         ts.setVisible(false);
         if (p != null && ts != null) {
-            if (!OmniSystemUIUtils.isXMasFunEnabled()) {
+            if (!OmniSystemUIUtils.isNewYearsFunEnabled()) {
                 p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                     @Override
                     public boolean onPreferenceClick(Preference preference) {

NoSpamDan/frameworks_base

packages/SystemUI/src/com/android/systemui/candy/GifImageView.java
@@ -1,0 +1,93 @@
+package com.android.systemui.candy;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.net.Uri;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class GifImageView extends View {
+
+    private InputStream mInputStream;
+    private Movie mMovie;
+    private int mWidth, mHeight;
+    private long mStart;
+    private Context mContext;
+
+    public GifImageView(Context context) {
+        super(context);
+        this.mContext = context;
+    }
+
+    public GifImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GifImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.mContext = context;
+        if (attrs.getAttributeName(1).equals("background")) {
+            int id = Integer.parseInt(attrs.getAttributeValue(1).substring(1));
+            setGifImageResource(id);
+        }
+    }
+
+    private void init() {
+        setFocusable(true);
+        mMovie = Movie.decodeStream(mInputStream);
+        mWidth = mMovie.width();
+        mHeight = mMovie.height();
+
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(mWidth, mHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        long now = SystemClock.uptimeMillis();
+
+        if (mStart == 0) {
+            mStart = now;
+        }
+
+        if (mMovie != null) {
+
+            int duration = mMovie.duration();
+            if (duration == 0) {
+                duration = 1000;
+            }
+
+            int relTime = (int) ((now - mStart) % duration);
+
+            mMovie.setTime(relTime);
+
+            mMovie.draw(canvas, 0, 0);
+            invalidate();
+        }
+    }
+
+    public void setGifImageResource(int id) {
+        mInputStream = mContext.getResources().openRawResource(id);
+        init();
+    }
+
+    public void setGifImageUri(Uri uri) {
+        try {
+            mInputStream = mContext.getContentResolver().openInputStream(uri);
+            init();
+        } catch (FileNotFoundException e) {
+            Log.e("GIfImageView", "File not found");
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -44,6 +44,7 @@ import com.android.systemui.R.id;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.candy.SystemUIUtils;
 import com.android.systemui.candy.xFallView.views.XFallView;
+import com.android.systemui.candy.GifImageView;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.qs.customize.QSCustomizer;
 import com.android.systemui.statusbar.CommandQueue;
@@ -85,19 +86,17 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
     private RemoteInputQuickSettingsDisabler mRemoteInputQuickSettingsDisabler =
             Dependency.get(RemoteInputQuickSettingsDisabler.class);
 
-<<<<<<< HEAD
     private boolean mSecureExpandDisabled;
-=======
+
     private View mFunImage;
     private View mFunImageContainer;
-    private XFallView mXFallView;
+    private GifImageView mFunBgImage;
     private boolean mForceHideFun;
     private boolean mForceShowFun;
     private ViewPropertyAnimator mFunAnimation;
 
     private static final String QS_SHOW_FUN = "qs_show_fun";
     private static final String QS_FORCE_SHOW_FUN = "qs_force_show_fun";
->>>>>>> 61e8afb0436... SystemUI: add handling of special features
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -116,7 +115,7 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
         mContainer = view.findViewById(id.quick_settings_container);
         mFunImage = view.findViewById(R.id.qs_fun_image);
         mFunImageContainer = view.findViewById(R.id.qs_fun_image_container);
-        mXFallView = view.findViewById(R.id.qs_fun_background);
+        mFunBgImage = view.findViewById(R.id.qs_fun_background);
         mQSDetail.setQsPanel(mQSPanel, mHeader, (View) mFooter);
         mQSAnimator = new QSAnimator(this,
                 mHeader.findViewById(R.id.quick_qs_panel), mQSPanel);
@@ -317,7 +316,6 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             showFunImage();
         } else {
             mFunImageContainer.setVisibility(View.GONE);
-            mXFallView.stopFall();
             if (mFunAnimation != null) {
                 mFunAnimation.cancel();
             }
@@ -493,8 +491,6 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             updateQsState();
         }
     };
-<<<<<<< HEAD
-=======
 
     public QuickQSPanel getQuickQsPanel() {
         return mQuickQSPanel;
@@ -506,34 +502,14 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             return;
         }
         mFunImageContainer.setVisibility(View.VISIBLE);
-        if (!mQsExpanded) {
-            mFunImage.setVisibility(View.VISIBLE);
-            mFunImage.setTranslationX(0);
-            mFunAnimation = mFunImage.animate()
-                .translationX(mQSPanel.getWidth() - mFunImage.getWidth())
-                .setInterpolator(Interpolators.LINEAR)
-                .setStartDelay(0)
-                .setDuration(5000)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        mFunImage.setVisibility(View.GONE);
-                    }
-                });
-            mFunAnimation.start();
-        }
-        mXFallView.startFall();
-        mXFallView.animate()
-            .setDuration(500)
-            .alpha(1)
-            .start();
+        mFunBgImage.setGifImageResource(R.raw.fireworks);
     }
 
     private boolean isFunEnabled() {
         if (!SystemUIUtils.isFunEnabled()) {
             return false;
         }
-        if(SystemUIUtils.isXMasFunEnabled()) {
+        if(SystemUIUtils.isNewYearsFunEnabled()) {
             if (mForceHideFun) {
                 return false;
             } else {
@@ -557,19 +533,19 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             mForceShowFun = newValue != null && Integer.parseInt(newValue) == 1;
         }
         if (isFunEnabled()) {
-            mXFallView.setOnLongClickListener(new View.OnLongClickListener(){
+            mFunBgImage.setOnLongClickListener(new View.OnLongClickListener(){
                 @Override
                 public boolean onLongClick(View v) {
                     if (!mKeyguardShowing) {
-                        SystemUIUtils.startXMasFun(getContext());
+                        SystemUIUtils.startsNewYearsFun(getContext());
                     }
                     return true;
                 }
             });
-            mXFallView.setOnClickListener(new View.OnClickListener(){
+            mFunBgImage.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
-                    final MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.ho_ho_ho);
+                    final MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.fireworks_sound);
                     mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                         @Override
                         public void onCompletion(MediaPlayer mp){
@@ -581,5 +557,4 @@ public class QSFragment extends Fragment implements QS, CommandQueue.Callbacks, 
             });
         }
     }
->>>>>>> 61e8afb0436... SystemUI: add handling of special features
 }

packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -99,7 +99,7 @@ public class TunerFragment extends PreferenceFragment {
 
         Preference preference = findPreference(KEY_QS_SHOW_FUN);
         if (preference != null) {
-            if (!SystemUIUtils.isXMasFunEnabled()) {
+            if (!SystemUIUtils.isNewYearsFunEnabled()) {
                 ((PreferenceScreen) findPreference(KEY_QS_CATEGORY)).removePreference(preference);
             }
         }
@@ -107,7 +107,7 @@ public class TunerFragment extends PreferenceFragment {
         final Preference ts = findPreference(KEY_QS_FORCE_SHOW_FUN);
         ts.setVisible(false);
         if (p != null && ts != null) {
-            if (!SystemUIUtils.isXMasFunEnabled()) {
+            if (!SystemUIUtils.isNewYearsFunEnabled()) {
                 p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                     @Override
                     public boolean onPreferenceClick(Preference preference) {