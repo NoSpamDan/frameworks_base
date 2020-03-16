dcd225da93d68aa21004ca1e9b05a84f38e4fe69
Nicolas Dhouailly <ndhouailly@gmail.com>
2020-03-16 16:05:03 +0100

    Navbar: Restore SystemUI visibility after enabling navbar
    Credit to Bruno Martins <bgcngm@gmail.com> for code used from
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/6007868142b2204445c649c335dff4feaf2f5d1d
    
    However, said code was implemented differently to serve our needs for the existing navbar
    visibility toggle.
    
    Change-Id: If57cf843996aaabf7aa2ff5bf0d9d5d3efad2aee

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -699,6 +699,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         mUiModeManager = mContext.getSystemService(UiModeManager.class);
         mKeyguardViewMediator = getComponent(KeyguardViewMediator.class);
+        mNavigationBarSystemUiVisibility = mNavigationBarController.createSystemUiVisibility();
         mActivityIntentHelper = new ActivityIntentHelper(mContext);
         final KeyguardSliceProvider sliceProvider = KeyguardSliceProviderGoogle.getAttachedInstance();
         if (sliceProvider != null) {
@@ -959,7 +960,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mNotificationLogger.setHeadsUpManager(mHeadsUpManager);
         putComponent(HeadsUpManager.class, mHeadsUpManager);
 
-        updateNavigationBar(true);
+        updateNavigationBar(result, true);
 
         if (ENABLE_LOCKSCREEN_WALLPAPER && mWallpaperSupported) {
             mLockscreenWallpaper = new LockscreenWallpaper(mContext, this, mHandler);
@@ -1994,7 +1995,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     uri.equals(Settings.System.getUriFor(Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION))) {
                 setGestureNavOptions();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.FORCE_SHOW_NAVBAR))) {
-                updateNavigationBar(false);
+                updateNavigationBar(getRegisterStatusBarResult(), false);
             }
         }
 
@@ -2458,6 +2459,16 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (displayId != mDisplayId) {
             return;
         }
+
+        mNavigationBarSystemUiVisibility.displayId = displayId;
+        mNavigationBarSystemUiVisibility.vis = vis;
+        mNavigationBarSystemUiVisibility.fullscreenStackVis = fullscreenStackVis;
+        mNavigationBarSystemUiVisibility.dockedStackVis = dockedStackVis;
+        mNavigationBarSystemUiVisibility.mask = mask;
+        mNavigationBarSystemUiVisibility.fullscreenStackBounds = fullscreenStackBounds;
+        mNavigationBarSystemUiVisibility.dockedStackBounds = dockedStackBounds;
+        mNavigationBarSystemUiVisibility.navbarColorManagedByIme = navbarColorManagedByIme;
+
         final int oldVal = mSystemUiVisibility;
         final int newVal = (oldVal&~mask) | (vis&mask);
         final int diff = newVal ^ oldVal;
@@ -4697,6 +4708,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             Dependency.get(DeviceProvisionedController.class);
 
     protected NavigationBarController mNavigationBarController;
+    private NavigationBarController.SystemUiVisibility mNavigationBarSystemUiVisibility;
 
     // UI-specific methods
 
@@ -5083,16 +5095,26 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mStatusBarMode;
     }
 
-    private void updateNavigationBar(boolean init) {
+    private RegisterStatusBarResult getRegisterStatusBarResult() {
+        RegisterStatusBarResult result = null;
+        try {
+            result = mBarService.registerStatusBar(mCommandQueue);
+        } catch (RemoteException ex) {
+            ex.rethrowFromSystemServer();
+        }
+        return result;
+    }
+
+    private void updateNavigationBar(@Nullable RegisterStatusBarResult result, boolean init) {
         boolean showNavBar = CandyUtils.deviceSupportNavigationBar(mContext);
         if (init) {
             if (showNavBar) {
-                mNavigationBarController.createNavigationBars(true, null);
+                mNavigationBarController.createNavigationBars(true, result);
             }
         } else {
             if (showNavBar != mShowNavBar) {
                 if (showNavBar) {
-                    mNavigationBarController.createNavigationBars(true, null);
+                    mNavigationBarController.recreateNavigationBars(true, result, mNavigationBarSystemUiVisibility);
                 } else {
                     mNavigationBarController.removeNavigationBar(mDisplayId);
                 }
