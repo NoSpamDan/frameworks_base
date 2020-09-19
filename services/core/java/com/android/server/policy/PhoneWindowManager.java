DirtyUnicorns/android_frameworks_base

Navbar: Add Expand notifications action
Change-Id: I70878ba8815af82edbe856ef39c832e0b8206a8b

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6484,6 +6484,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_SCREENSHOT:
                 toggleScreenshot();
                 break;
+            case NavbarUtilities.KEY_ACTION_NOTIFICATIONS:
+                toggleNotifications();
+                break;
         }
     }
 
@@ -6569,5 +6572,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void toggleScreenshot() {
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Screenshot");
         ActionUtils.takeScreenshot(true);
+    }
+
+    // Notifications
+    private void toggleNotifications() {
+        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Notifications");
+        ActionUtils.toggleNotifications();
     }
 }

NoSpamDan/frameworks_base

Change-Id: Ib60af36ea449880d692ec6cbbccaaa49270cceba

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6576,6 +6576,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_SCREENSHOT:
                 toggleScreenshot();
                 break;
+            case NavbarUtilities.KEY_ACTION_NOTIFICATIONS:
+                toggleNotifications();
+                break;
         }
     }
 
@@ -6661,5 +6664,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void toggleScreenshot() {
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Screenshot");
         aosipUtils.takeScreenshot(true);
+    }
+
+    // Notifications
+    private void toggleNotifications() {
+        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Notifications");
+        aosipUtils.toggleNotifications();
     }
 }